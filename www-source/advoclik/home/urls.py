36b3953609355e706a67d0e7252f24b6efe3e9a1
pkravik <pkravik@gmail.com>
2016-02-08 14:53:07 -0600

    fixed staticfile reference

--- www-source/advoclik/home/urls.py
+++ www-source/advoclik/home/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^initialize', views.index, name='initialize'),
     url(r'refer/(?P<url_suffix>.*)/$', views.referral_redirect, name='referral_link'),
-    url(r'^campaigns/$', views.campaigns, name='campaigns'),
-    url(r'^campaigns/results/(?P<campaign_id>[0-9]+)/$', views.campaign_summary, name='campaign_click_summary')
+    url(r'campaigns/$', views.campaigns, name='campaigns'),
+    url(r'campaigns/results/(?P<campaign_id>[0-9]+)/$', views.campaign_summary, name='campaign_click_summary')
 ]
